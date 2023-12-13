6a51328bb2daba72d9639dead5f3dd5865562ae4
Ekele james <ekeleejames@gmail.com>
2023-12-13 19:11:09 +0100

    Update hadoop_cluster.rb

--- cookbooks/hadoop_cluster/attributes/hadoop_cluster.rb
+++ cookbooks/hadoop_cluster/attributes/hadoop_cluster.rb
@@ -1,17 +1,3 @@
-#
-#   Portions Copyright (c) 2012-2014 VMware, Inc. All Rights Reserved.
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-#
 
 default[:hadoop][:install_from_tarball] = true # install hadoop components using tarball or rpm
 default[:hadoop][:is_hadoop_yarn] = false # is deploying a Hadoop version with YARN (e.g hadoop 0.23) ?
